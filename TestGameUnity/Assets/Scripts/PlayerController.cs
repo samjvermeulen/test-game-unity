93184ea049782ef59905b21ca579128767e5cd1d
samjvermeulen <samjvermeulen@outlook.com>
2017-10-10 21:51:06 -0400

    Added player movement, prototyping world, and weapon holder

---
+++ TestGameUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Requires a PlayerMotor script.
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5.0f;
+
+	[SerializeField]
+	private float mouseSensitivity = 3.0f;
+
+	private PlayerMotor motor;
+
+	// Called when PlayerController is created.
+	// Gets the player motor component.
+	void Start() {
+		// Hide player cursor.
+		Cursor.visible = false;
+
+		motor = GetComponent<PlayerMotor> ();
+	}
+
+	// Called every frame of the game.
+	// Gets input from the player, then converts it
+	// into the movement velocity vector.
+	void Update() {
+		// Get the direction of movement from the input.
+		float xMov = Input.GetAxisRaw("Horizontal");
+		float zMov = Input.GetAxisRaw("Vertical");
+
+		// Create independant axis movement velocity vectors.
+		Vector3 movHorizontal = transform.right * xMov;
+		Vector3 movVertical = transform.forward * zMov;
+
+		// Create the movement velocity vector.
+		Vector3 velocity = (movHorizontal + movVertical) * speed;
+
+		// Create independant axis rotation float.
+		float xRot = Input.GetAxisRaw("Mouse Y");
+		float yRot = Input.GetAxisRaw("Mouse X");
+
+		// We apply rotation on y axis to the rigid body for turning.
+		// But for x axis rotation we only want the camera to move,
+		// not the entire body.
+
+		// Create y rotation vector with mouse sensitivity applied.
+		Vector3 rotation = new Vector3 (0.0f, yRot, 0.0f) * mouseSensitivity;
+
+		// Create x rotation vector with mouse sensitivity applied.
+		float camRotation = xRot * mouseSensitivity;
+
+		// Apply the movement, rotation, and camera rotation.
+		motor.SetVelocity(velocity);
+		motor.SetRotation(rotation);
+		motor.SetCameraRotation(camRotation);
+	}
+}
---
+++ TestGameUnity/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Requires a Rigidbody component.
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+	
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private float camRotationX = 0.0f;
+	private float currentCamRotationX = 0.0f;
+
+	[Range(0.0f, 90.0f)]
+	public float cameraAngleLimit = 0.0f;
+
+	// Reference components.
+	private Rigidbody body;
+
+	// This field is optional. Will only apply
+	// rotation on y axis if it is not referenced.
+	[SerializeField]
+	private Camera cam;
+
+	// Called when the PlayerMotor is created.
+	// Gets the rigid body component.
+	// Sets the distance to ground.
+	void Start()
+	{
+		body = GetComponent<Rigidbody>();
+	}
+
+	// Set the velocity to the velocity we
+	// calculated in the PlayerController.
+	public void SetVelocity(Vector3 pvelocity) {
+		velocity = pvelocity;
+	}
+
+	// Set the rotation to the rotation we
+	// calculated in the PlayerController.
+	public void SetRotation(Vector3 protation) {
+		rotation = protation;
+	}
+
+	// Set the camera rotation to the camera rotation
+	// we calculate in the PlayerController.
+	public void SetCameraRotation(float pcamRotationX) {
+		camRotationX = pcamRotationX;
+	}
+
+	// Runs every physics iteration.
+	void FixedUpdate() {
+		Move();
+		Rotate();
+
+		// If there is not camera, only apply 
+		// rotation to the y axis.
+		if (cam != null) {
+			RotateCamera();
+		}
+	}
+
+	// Apply the velocity to the player.
+	void Move() {
+		// Do not move player if the velocity is zero.
+		if (velocity != Vector3.zero) {
+			// Will not move player into wall.*
+			body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	// Apply the rotation to the player.
+	void Rotate() {
+		// Do not rotate player if rotation is zero.
+		if (rotation != Vector3.zero) {
+			body.MoveRotation(body.rotation * Quaternion.Euler(rotation));
+		}
+	}
+
+	// Apply the rotation to the camera.
+	void RotateCamera() {
+		// Do not rotate camera is rotation is zero.
+		if (camRotationX != 0.0f) {
+			currentCamRotationX -= camRotationX;
+
+			// Clamp rotation to the camera angle limit. 
+			// So we cannot look all the way over our head, and
+			// under our feet.
+			currentCamRotationX = Mathf.Clamp(currentCamRotationX, -cameraAngleLimit, cameraAngleLimit);
+
+			// Apply the rotation.
+			cam.transform.localEulerAngles = new Vector3(currentCamRotationX, 0.0f, 0.0f);
+		}
+	}
+}
